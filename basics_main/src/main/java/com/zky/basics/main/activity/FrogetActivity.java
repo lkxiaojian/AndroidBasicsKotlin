c47cf5a34375a545f6e7bf4eb7aa9162c384b7b8
lk <18513075404@163.com>
2020-08-15 16:50:37 +0800

    main

---
+++ basics_main/src/main/java/com/zky/basics/main/activity/FrogetActivity.java
@@ -0,0 +1,54 @@
+package com.zky.basics.main.activity;
+
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.zky.basics.common.BR;
+import com.zky.basics.common.mvvm.BaseMvvmActivity;
+import com.zky.basics.main.R;
+import com.zky.basics.main.databinding.ActivityLoginBinding;
+import com.zky.basics.main.mvvm.factory.MainViewModelFactory;
+import com.zky.basics.main.mvvm.viewmodel.SplashViewModel;
+
+public class FrogetActivity extends BaseMvvmActivity<ActivityLoginBinding, SplashViewModel> {
+    @Override
+    public Class<SplashViewModel> onBindViewModel() {
+        return SplashViewModel.class;
+    }
+
+    @Override
+    public ViewModelProvider.Factory onBindViewModelFactory() {
+        return MainViewModelFactory.getInstance(getApplication());
+    }
+
+    @Override
+    public void initViewObservable() {
+        mViewModel.getCaptcha();
+        mViewModel.getmVoidSingleLiveEvent().observe(this, aVoid -> finishActivity());
+    }
+
+    @Override
+    public int onBindVariableId() {
+        return BR.forgetViewModel;
+    }
+
+    @Override
+    public int onBindLayout() {
+        return R.layout.activity_froget;
+    }
+
+
+    @Override
+    public int onBindToolbarLayout() {
+        return R.layout.white_common_toolbar;
+    }
+
+    @Override
+    public String getTootBarTitle() {
+        return "重置密码";
+    }
+    @Override
+    public boolean isFullScreen() {
+        return true;
+    }
+
+}
---
+++ basics_main/src/main/java/com/zky/basics/main/activity/SplashActivity.kt
@@ -0,0 +1,106 @@
+package com.zky.basics.main.activity
+
+import android.annotation.SuppressLint
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.databinding.ViewDataBinding
+import android.os.Handler
+import android.os.Message
+import com.alibaba.android.arouter.facade.Postcard
+import com.alibaba.android.arouter.facade.callback.NavigationCallback
+import com.alibaba.android.arouter.launcher.ARouter
+import com.zky.basics.common.mvvm.BaseMvvmActivity
+import com.zky.basics.common.view.ARouterPath
+import com.zky.basics.main.R
+import com.zky.basics.main.mvvm.factory.MainViewModelFactory
+import com.zky.basics.main.mvvm.viewmodel.SplashViewModel
+import java.lang.ref.WeakReference
+
+class SplashActivity : BaseMvvmActivity<ViewDataBinding, SplashViewModel>() {
+    override fun onBindLayout() = R.layout.activity_splash
+    private var handler: CustomHandler? = null
+    @SuppressLint("CheckResult")
+    override fun initView() {
+        handler = WeakReference(CustomHandler()).get()
+        handler?.sendEmptyMessageDelayed(1, 800)
+//        RxPermissions(this).request(
+//            Manifest.permission.READ_EXTERNAL_STORAGE,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE
+//        ).subscribe { permission: Boolean ->
+//                if (permission) {
+//                    handler!!.sendEmptyMessageDelayed(1, 2000)
+//                    //同意sd 权限后创建room 数据库
+//                    val testRoomDbDao =
+//                        AppDatabase.getDatabase(this)?.testRoomDbDao()
+//                    val testRoomDb = TestRoomDb(223, "name", 3, "1", "3")
+//                    testRoomDbDao?.insertAll(testRoomDb)
+//                    val all = testRoomDbDao?.all
+//                    all?.forEach {
+//                        Log.e("tag","${it?.u_id}")
+//                    }
+//                } else {
+//                    ToastUtil.showToast("读取sd卡权限被拒绝")
+//                }
+//            }
+    }
+
+    override fun enableToolbar(): Boolean {
+        return false
+    }
+
+    fun startMainActivity() {
+        ARouter
+            .getInstance()
+            .build(ARouterPath.LOGIN)
+            .withTransition(R.anim.fade_in, 0)
+            .navigation(this, object : NavigationCallback {
+                override fun onLost(postcard: Postcard?) {
+                    //没有找到
+
+
+                }
+
+                override fun onFound(postcard: Postcard?) {
+                    //找到路由
+                }
+
+                override fun onInterrupt(postcard: Postcard?) {
+                    //使用拦截器的时候
+                }
+
+                override fun onArrival(postcard: Postcard?) {
+                    //向android 发出startActivity 请求
+                    finishActivity()
+                }
+
+            })
+//            .navigation()
+//        finishActivity()
+    }
+
+    override fun onBindViewModel(): Class<SplashViewModel> = SplashViewModel::class.java
+    override fun onBindViewModelFactory(): ViewModelProvider.Factory? =
+        MainViewModelFactory.getInstance(application)
+
+    override fun initViewObservable() {
+        mViewModel!!.getmVoidSingleLiveEvent()
+            .observe(this, Observer { startMainActivity() })
+    }
+
+    override fun onBindVariableId() = 0
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler?.removeCallbacksAndMessages(null)
+        handler = null
+    }
+
+    override val isFullScreen: Boolean = true
+
+    inner class CustomHandler : Handler() {
+        override fun handleMessage(msg: Message?) {
+            super.handleMessage(msg)
+            startMainActivity()
+        }
+    }
+}
