9abc4d5c39c3cd6f3fa2500d86aed398a11975b4
lk <18513075404@163.com>
2020-08-16 09:37:01 +0800

    Kotlin data Class 无参构造实例测试

---
+++ lib_zky_api/src/main/java/com/zky/basics/api/splash/entity/TestE.kt
@@ -0,0 +1,12 @@
+package com.zky.basics.api.splash.entity
+
+import com.zky.basics.annotation.AllowNoArg
+
+/**
+ * Created by lk
+ * Date 2020/8/16
+ * Time 08:51
+ * Detail: kotlin 无参构造 测试类
+ */
+@AllowNoArg
+data class TestE(var value: String)
