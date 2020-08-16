120a5ffc65d01a0d2b7889f25a2e1969ba88c18d
lk <18513075404@163.com>
2020-08-16 11:18:09 +0800

    Kotlin 扩展函数

---
+++ lib_common/src/main/java/com/zky/basics/common/util/spread/StringSpread.kt
@@ -0,0 +1,28 @@
+package com.zky.basics.common.util.spread
+
+/**
+ * Created by lk
+ * Date 2020/8/16
+ * Time 09:48
+ * Detail:
+ */
+
+/**
+ * 字符串最后一个字符
+ */
+fun String.lastString(): String = if (this.isNullOrEmpty()) "" else this[this.length - 1].toString()
+
+/**
+ * 字符串第一个字符
+ */
+fun String.firstString(): String = if (this.isNullOrEmpty()) "" else  this[0].toString()
+
+/**
+ * 去掉最后一个字符
+ */
+fun String.removeLastString(): String =  if (this.isNullOrEmpty()) "" else this.substring(0, this.length - 1)
+
+
+
+
+
